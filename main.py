0fbb337ebf24b82a349a0dbb8ac255f3c4882e8c
Calin <costea.calin@gmail.com>
2020-12-01 13:16:22 +0200

    Cerinta 1

---
+++ main.py
@@ -0,0 +1,148 @@
+import pymongo
+from pprint import pprint
+import pandas as pd
+conn = pymongo.MongoClient("mongodb://master:stud1234@37.120.249.57:27017/?authSource=daune_leasing&authMechanism=SCRAM-SHA-256")
+db = conn["daune_leasing"]
+print(db.list_collection_names())
+
+clienti_leasing = db["clienti_leasing"]
+clienti_daune = db["clienti_daune"]
+
+#cerinta 1 - interogari simple
+
+#1
+projection={"_id":0,
+            "NUME_CLIENT":1,
+            "PROFESIA":1,
+            "VARSTA":1,
+            "SUMA_DEPOZIT":1}
+
+cursor=clienti_leasing.find({},projection=projection)
+set_clienti=list(cursor)
+#pprint (set_clienti)
+cursor.close()
+
+#2
+projection={"_id":0,
+            "DAUNA":1,
+            "VALOARE_DAUNA":1}
+sort=[("VALOARE_DAUNA", -1)]
+cursor=clienti_daune.find({}, projection=projection, sort=sort);
+set_clienti=list(cursor)
+#pprint (set_clienti)
+cursor.close()
+
+#3
+projection={"_id":0,
+            "TARAPRODUCATOR":1,
+            "REGIUNEPRODUCATOR":1,
+            "MARCA":1,
+            "MODEL":1,
+            "VALOARE_DAUNA":1
+            }
+sort=[("MARCA", 1)]
+cursor=clienti_daune.find({}, projection=projection, sort=sort);
+set_clienti=list(cursor)
+#pprint (set_clienti)
+cursor.close()
+
+#4
+projection={"_id":0,
+            "NUME_CLIENT":1,
+            "PROFESIA":1,
+            "SUMA_SOLICITATA":1}
+sort=[("VARSTA", 1)]
+cursor=clienti_leasing.find({"FIDELITATE": 2},projection=projection, sort=sort)
+set_clienti=list(cursor)
+#pprint (set_clienti)
+cursor.close()
+
+#5
+projection={"_id":0,
+            "NUME_CLIENT":1,
+            "PROFESIA":1,
+            "SUMA_DEPOZIT":1}
+cursor=clienti_leasing.find({"SUMA_DEPOZIT": {"$gt": 10000}},projection=projection)
+set_clienti=list(cursor)
+#pprint (set_clienti)
+cursor.close()
+
+#cerinta 1 - agregari
+
+#1
+pipeline=[{'$group' : {
+           "_id" : "$PROFESIA",
+           "VAL_CREDIT_TOTAL": { "$sum": "$VAL_CREDITE_RON" }
+            }},
+        { '$sort': {"_id": 1}}]
+cursor=clienti_leasing.aggregate(pipeline)
+df = pd.DataFrame.from_dict(list(cursor))
+cursor.close()
+#pprint(df)
+
+#2
+pipeline=[{ "$match": { "VARSTA": { "$gt": 30, "$lt": 40 } } },
+{ "$group": { "_id": "$STARE_CIVILA", "NUMAR": { "$sum": 1 }, "MEDIE": { "$avg": "$SUMA_SOLICITATA" } } }]
+cursor=clienti_leasing.aggregate(pipeline)
+df = pd.DataFrame.from_dict(list(cursor))
+cursor.close()
+#pprint(df)
+
+#3
+pipeline=[
+    {
+        "$bucket": {
+            "groupBy": "$SUMA_SOLICITATA",
+            "boundaries": [0, 1000, 5000, 10000, 20000, 50000],
+            "default": "Other",
+            "output": {
+                "Nr_total": { "$sum": 1 },
+                "Clienti leasing": { "$push": { "NUME_CLIENT": "$NUME_CLIENT", "MONEDA": "$MONEDA", "VARSTA": "$VARSTA", "SUMA_SOLICITATA": "$SUMA_SOLICITATA" } }
+            }
+        }
+    }
+]
+cursor=clienti_leasing.aggregate(pipeline)
+df = pd.DataFrame.from_dict(list(cursor))
+cursor.close()
+#pprint(df)
+
+#4
+pipeline=[{
+        "$facet": {
+            "Starea civila":
+                [{
+                    "$unwind": "$STARE_CIVILA"
+                }, { "$sortByCount": "$STARE_CIVILA" }],
+            "Sex": [{
+                    "$unwind": "$SEX"
+                }, { "$sortByCount": "$SEX" }]
+        }
+    }]
+cursor=clienti_leasing.aggregate(pipeline)
+df = pd.DataFrame.from_dict(list(cursor))
+cursor.close()
+#pprint(df)
+
+#5
+pipeline=[{'$group' : {
+           "_id" : "$PROFESIA",
+            "VARSTA_MEDIE": { "$avg": "$VARSTA" }
+            }},
+        { '$sort': {"_id": 1}}]
+cursor=clienti_leasing.aggregate(pipeline)
+df = pd.DataFrame.from_dict(list(cursor))
+cursor.close()
+#pprint(df)
+
+#cerinta 2
+
+
+
+
+
+
+
+
+
+
